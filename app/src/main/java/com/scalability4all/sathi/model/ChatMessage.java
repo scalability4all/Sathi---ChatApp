a22a933eae40954aa8c778c91aa32ae61a3dad18
Satyam Mittal <satyam.mittal@grabtaxi.com>
2020-05-08 22:38:14 +0800

    removed hostname from jabber id to make username to show in app

--- app/src/main/java/com/scalability4all/sathi/model/ChatMessage.java
+++ app/src/main/java/com/scalability4all/sathi/model/ChatMessage.java
@@ -11,8 +11,8 @@ public class ChatMessage {
     private String toContactJid;
     private int persistID;
     public static final String TABLE_NAME = "chatMessages";
-    public static final class Cols
-    {
+
+    public static final class Cols {
         public static final String CHAT_MESSAGE_UNIQUE_ID = "chatMessageUniqueId";
         public static final String MESSAGE = "message";
         public static final String TIMESTAMP = "timestamp";
@@ -20,34 +20,43 @@ public class ChatMessage {
         public static final String FROM_CONTACT_JID = "fromContactjid";
         public static final String TO_CONTACT_JID = "toContactjid"; // to here represents before version of contactJid
     }
-    public ChatMessage(String message, long timestamp, Type type , String toContactJid,String fromContactJid){
+
+    public ChatMessage(String message, long timestamp, Type type, String toContactJid, String fromContactJid) {
         this.message = message;
         this.timestamp = timestamp;
         this.type = type;
         this.fromContactJid = fromContactJid;
         this.toContactJid = toContactJid;
     }
+
     public String getMessage() {
         return message;
     }
+
     public long getTimestamp() {
         return timestamp;
     }
+
     public Type getType() {
         return type;
     }
+
     public void setMessage(String message) {
         this.message = message;
     }
+
     public void setTimestamp(long timestamp) {
         this.timestamp = timestamp;
     }
+
     public void setType(Type type) {
         this.type = type;
     }
+
     public int getPersistID() {
         return persistID;
     }
+
     public void setPersistID(int persistID) {
         this.persistID = persistID;
     }
@@ -68,24 +77,24 @@ public class ChatMessage {
         this.toContactJid = toContactJid;
     }
 
-    public String getTypeStringValue(Type type)
-    {
-        if(type== Type.SENT)
+    public String getTypeStringValue(Type type) {
+        if (type == Type.SENT)
             return "SENT";
         else
             return "RECEIVED";
     }
-    public ContentValues getContentValues()
-    {
+
+    public ContentValues getContentValues() {
         ContentValues values = new ContentValues();
         values.put(Cols.MESSAGE, message);
         values.put(Cols.TIMESTAMP, timestamp);
-        values.put(Cols.MESSAGE_TYPE,getTypeStringValue(type));
-        values.put(Cols.FROM_CONTACT_JID,fromContactJid);
-        values.put(Cols.TO_CONTACT_JID,toContactJid);
+        values.put(Cols.MESSAGE_TYPE, getTypeStringValue(type));
+        values.put(Cols.FROM_CONTACT_JID, fromContactJid);
+        values.put(Cols.TO_CONTACT_JID, toContactJid);
         return values;
     }
-    public enum  Type {
-        SENT,RECEIVED
+
+    public enum Type {
+        SENT, RECEIVED
     }
 }
